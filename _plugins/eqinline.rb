987b4bd19e85b870d33000938da595c7d8e72401
jhPark <86312443+jhparkland@users.noreply.github.com>
2023-03-02 18:31:38 +0900

    Update eqinline.rb

--- _plugins/eqinline.rb
+++ _plugins/eqinline.rb
@@ -7,20 +7,23 @@ module Jekyll
 			super
 			@markup = "#{markup}".strip
 		end
+
 		def render(context)
 
 			require 'execjs'
 
 			parsed = Liquid::Template.parse(@markup).render context
 			
-			katexsrc = open("./js/katex.min.js").read
+			katexsrc = open("js\katex.min.js").read
 			@katex = ExecJS.compile(katexsrc)
 			return eqn_to_html(parsed)
 
 		end
+
 		def eqn_to_html(string)
 			return @katex.call("katex.renderToString", string)
 		end
+
 	end
 end
 
